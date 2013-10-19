b708036468dd8f2212118dc0dcd32dc9bdabe878
Andrew <andrew.kogler@utexas.edu>
2013-10-19 04:10:29 -0400

    Support for point incrementation.

--- mysql_service.py
+++ mysql_service.py
@@ -9,16 +9,16 @@ db="heroku_62b297a4786cfb1")
 cursor = connection.cursor()
 
 def get_user(user_data):
-	username = user_data["username"]
-	password = user_data["password"]
+	username = user_data['username']
+	password = user_data['password']
 	result = cursor.execute("SELECT name FROM users WHERE name='" + username + "' && password='" + password + "'")
 	if result:
 		return username 
 	return None
 
 def register_user(user_data):
-	username = user_data["username"]
-	password = user_data["password"]
+	username = user_data['username']
+	password = user_data['password']
 	email = user_data["email"]
 	result = cursor.execute("SELECT email FROM users WHERE email='" + email + "'")
 	if result:
@@ -27,4 +27,15 @@ def register_user(user_data):
 	else:
 		cursor.execute("INSERT INTO users (name, password, email) VALUES('" + username 
 		+ "', '" + password + "', '" + email +"')")
+		return True
+
+def increment_points(user_data, pointAmount):
+	username = user_data['username']
+	password = user_data['password']
+	email = user_data['email']
+	result = cursor.execute("UPDATA users SET points + " + pointAmount + " WHERE email = '" + email + "'")
+	if !result:
+		return False
+
+	else:
 		return True
