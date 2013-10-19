e69deb69d409bc83d1f9ecadc30155813d1fee01
Don Yu <dy2212@columbia.edu>
2013-10-19 01:23:27 -0400

    heroku directory fixes

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+	if 'username' in session:
+		return render_template('dashboard.html')
+	else:
+		return render_template('index.html')
 
-#@app.route('/login', methods=['POST'])
-#def login():
-#    pass
+@app.route('/login', methods=['POST'])
+def login():
+	pass
+
+@app.route('/register', methods=['POST'])
+def register():
+	pass
     
 if __name__ == '__main__':
     app.run(port=8080)
