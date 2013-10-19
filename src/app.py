59863553fe90922077e3cf806dc5f4356a1dc6cc
Don Yu <dy2212@columbia.edu>
2013-10-19 00:03:00 -0400

    returning templates

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+#@app.route('/login', methods=['POST'])
+#def login():
+#    pass
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(port=8080)
